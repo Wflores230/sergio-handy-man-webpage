90e8b9c996d83a73069e60491e9e080c2a71a0d8
William Flores <floreswilliam891@hotmail.com>
2022-07-28 18:47:18 -0400

    added button component with css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,23 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+import { Button } from './Button';
 
 function Navbar() {
     const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
 
     const  handleClick = () => setClick(!click);
-
     const closeMobileMenu = () => setClick(false);
+
+    const showButton = () => {
+        if(window.innerWidth <= 960){
+            setButton(false)
+        } else {
+            setButton(true)
+        }
+    };
+
+    window.addEventListener('resize', showButton);
   return (
     <div>
         <nav className='navbar'>
@@ -39,7 +50,7 @@ function Navbar() {
                         </Link>
                     </li>
                 </ul>
-                
+                {button && <Button buttonStyle='btn--outline'>Services</Button>}
             </div>
         </nav>
     </div>
