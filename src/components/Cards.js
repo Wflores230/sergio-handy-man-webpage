85d8e5d9adffaaf99ccf7f0f6d2163b9e9db846c
William Flores <floreswilliam891@hotmail.com>
2022-08-16 17:57:08 -0400

    added cards, cards item, and cards css

---
+++ src/components/Cards.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import './Cards.css';
+import CardItem from './CardItem';
+
+function Cards() {
+  return (
+    <div className='cards'>
+      <h1>Services We Supply</h1>
+      <div className='cards__container'>
+        <div className='cards__wrapper'>
+          <ul className='cards__items'>
+            <CardItem
+              src='images/gutters.jpg'
+              text='Repairing damaged gutters'
+              label=''
+              path='/services'
+            />
+            <CardItem
+              src='images/dry_wall.jpg'
+              text='Drywall installation'
+              label=''
+              path='/services'
+            />
+          </ul>
+          <ul className='cards__items'>
+            <CardItem
+                src='images/painting.jpg'
+                text='Painting'
+                label=''
+                path='/services'
+            />
+            <CardItem
+              src='images/tiles.jpg'
+              text='Laying Tile'
+              label=''
+              path='/products'
+            />
+            <CardItem
+              src='images/retaining.jpg'
+              text='Retaining Walls'
+              label=''
+              path='/sign-up'
+            />
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Cards;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import Background from '../Background';
+import Cards from '../Cards'
 
 function Home() {
     return(
         <>
-            <Background></Background>
+            <Background/>
+            <Cards/>
+            
         </>
     );
     
