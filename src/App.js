36a73b58e8aa9fa745c5f76150122236c595dc77
William Flores <floreswilliam891@hotmail.com>
2022-08-27 01:27:05 -0400

    adding footer with footer css, added to homepage

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import './App.css';
 import Home from './components/pages/Home'
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 
 function App() {
@@ -13,6 +16,12 @@ function App() {
         <Routes>
           <Route path='/' exact element=
           { <Home/> } />
+          <Route path='/services' exact element=
+          { <Services/> } />
+          <Route path='/products' exact element=
+          { <Products/> } />
+          <Route path='/sign-up' exact element=
+          { <SignUp/> } />
         </Routes>
       </Router>
     </div>
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import '../../App.css';
 import Background from '../Background';
 import Cards from '../Cards'
+import Footer from '../Footer'
 
 function Home() {
     return(
         <>
             <Background/>
             <Cards/>
+            <Footer />
             
         </>
     );
